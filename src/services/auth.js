55537ea8e8ff6cb5f11bc48a884c7408c7688fce
percobaan <ranggayoviesaputra@gmail.com>
2025-04-21 12:21:04 +0700

    second commit

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000"
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 export const checkSession = async () => {
     const res = await fetch(`${BASE_URL}/auth/me`, {
